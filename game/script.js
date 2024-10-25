1e4d23ba9fc8020b8f103126ce4ae421d59272db
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2024-10-25 17:20:09 +0800

    fixed the code structure and change the round from 3 to 5 rounds per game

--- game/script.js
+++ game/script.js
@@ -1,55 +1,91 @@
-let choices = ["Rock","Paper","Scissor"], computerScore = 0,humanScore = 0, computerChoice = getComputerChoice(),  winner = false,wond = ""
+let choices = ["Rock","Paper","Scissor"], computerScore = 0,humanScore = 0,  winner = false,wond = ""
+var computerChoice = "", user = ""
+playGame();
 
+function getPlayerInput(){
+    return prompt("Choose command Rock or Paper or Scissor");
+
+}
 function getComputerChoice(){
-    let random =  Math.ceil(Math.random()*choices.length)
+    let random =  Math.ceil(Math.random()*choices.length-1)
     return choices[random];
 }
-while(!winner){
-    let user = prompt("Choose command Rock or Paper or Scissor");
-    if(computerChoice == "Rock" && user == "Scissor"){
-        console.log("Player choice: "+user+" Computer choice: "+computerChoice);
-        console.log("Computer scored!!")
-        computerScore++;
-        wond = "Computer"
-    
-    }else if(computerChoice == "Paper" && user == "Rock"){
-        console.log("Player choice: "+user+" Computer choice: "+computerChoice);
-        console.log("Computer scored!!");
-        computerScore++;
-        wond = "Computer"
-
-    }else if(computerChoice == "Scissor" && user == "Paper"){
-        console.log("Player choice: "+user+" Computer choice: "+computerChoice);
-        console.log("Computer scored!!");
-        computerScore++;
-        wond = "Computer"
-
-    }else if(computerChoice == "Rock" && user == "Rock"){   
-        console.log("Player choice: "+user+" Computer choice: "+computerChoice);
-        console.log("Draw!!");
-      
-    }else if(computerChoice == "Paper" && user == "Paper"){
-        console.log("Player choice: "+user+" Computer choice: "+computerChoice);
-        console.log("Draw!!");
-    }else if(computerChoice == "Scissor" && user == "Scissor"){
-        console.log("Player choice: "+user+" Computer choice: "+computerChoice);
-        console.log("Draw!!");
-    }else{
-        console.log("Player choice: "+user+" Computer choice: "+computerChoice);
-        console.log("Player scored!!");
-        humanScore++;
-        wond = "Player"
-    
+function checkWinner(humanScore,computerScore){
+    if(humanScore == 5 || computerScore == 5){
+       return true;
     }
-    console.log("Player score: "+humanScore)
-    console.log("Computer score: "+computerScore)
-    if(humanScore == 3 || computerScore == 3){
-        winner = true;
-        console.log(wond+" Wins!!")
-
-    }
+    return false;
 
 }
+function showCurrentScore(humanScore,computerScore){
+    console.log("Player Score:"+humanScore+"\nComputer Score:"+computerScore);
+
+}
+
+function declare(winner,wond){
+    if(winner){
+        console.log(wond+" Wins!!");
+        
+
+    }
+  
+}
+function playGame(){
+    user = getPlayerInput()
+    computerChoice = getComputerChoice();
+    let converted = user.toUpperCase()
+    let computerConverted = computerChoice.toUpperCase()
+   
+   if(computerConverted == "ROCK" && converted == "SCISSOR"){
+       console.log("Player choice: "+converted+" Computer choice: "+computerConverted);
+       console.log("Computer scored!!")
+       computerScore++;
+       wond = "Computer"
+   
+   }else if(computerConverted == "PAPER" && converted == "ROCK"){
+       console.log("Player choice: "+converted+" Computer choice: "+computerConverted);
+       console.log("Computer scored!!");
+       computerScore++;
+       wond = "Computer"
+
+   }else if(computerConverted == "SCISSOR" && converted == "PAPER"){
+       console.log("Player choice: "+converted+" Computer choice: "+computerConverted);
+       console.log("Computer scored!!");
+       computerScore++;
+       wond = "Computer"
+
+   }else if(computerConverted == "ROCK" && converted== "ROCK"){   
+       console.log("Player choice: "+converted+" Computer choice: "+computerConverted);
+       console.log("Draw!!");
+
+   }else if(computerConverted == "PAPER" && converted == "PAPER"){
+       console.log("Player choice: "+converted+" Computer choice: "+computerConverted);
+       console.log("Draw!!");
+
+   }else if(computerConverted == "SCISSOR" && converted == "SCISSOR"){
+       console.log("Player choice: "+converted+" Computer choice: "+computerConverted);
+       console.log("Draw!!");
+
+   }else{
+       console.log("Satisfied");
+       console.log("Player choice: "+converted+" Computer choice: "+computerConverted);
+       console.log("Player scored!!");
+       humanScore++;
+       wond = "Player"
+   }
+   
+  
+   showCurrentScore(humanScore,computerScore);
+   winner = checkWinner(humanScore,computerScore);
+   declare(winner,wond);
+   if(!winner){
+    playGame();
+
+   }
+  
+
+}
+
 
 
 
